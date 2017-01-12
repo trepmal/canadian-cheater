3869130f5b2eb63e1f3584957b13b6ae3cb91c7d
Kailey Lampert <trepmal@gmail.com>
2017-01-11 21:43:25 -0800

    formatting

--- canadian-cheater.php
+++ canadian-cheater.php
@@ -3,6 +3,8 @@
 
 add_filter( 'gettext', 'more_canadian', 10, 3 );
 function more_canadian( $translation, $text, $domain ) {
-	if ( $text != 'Cheatin&#8217; uh?' ) return $translation;
+	if ( 'Cheatin&#8217; uh?' != $text ) {
+		return $translation;
+	}
 	return 'Cheatin&#8217; eh?';
 }
